a0461dcf88cf007d664949f8b4c42024ced48d0a
nbpro <nbneeraj8@gmail.com>
2019-07-03 17:04:14 +0530

    Added header component

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DASHBOARD_ROUTING } from './dashboard.router';
+import { HeaderComponent } from '../core/components/header/header.component';
 
-// const DASHBOARD_IMPORTS = [DashBoardRoutingModule];
+const DASHBOARD_DECLARATIONS = [HeaderComponent, DashboardComponent];
 
 @NgModule({
   imports: [
     CommonModule,
     DASHBOARD_ROUTING
   ],
-  declarations: [DashboardComponent]
+  declarations: [...DASHBOARD_DECLARATIONS]
 })
 export class DashboardModule { }
